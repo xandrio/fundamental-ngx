ae8c75b99a3319d780cb473e8ba9fe78d168fa13
nikvalor <nikita.lastovych@valor-software.com>
2021-01-27 12:34:22 +0200

    Covered scenarios for grid list component (Core)

---
+++ e2e/wdio/core/fixtures/appData/grid-list-content.ts
@@ -0,0 +1,16 @@
+const text = 'Footer';
+const productTitle = 'Product';
+const warningColor = 'rgb(187,0,0)';
+const errorColor = 'rgb(233,115,12)';
+const successColor = 'rgb(16,126,62)';
+const neutralColor = 'rgb(106,109,112)';
+const backGroundColor = 'background-color';
+const color = 'color';
+const textLocked = 'Locked';
+const fontWeight = 'font-weight';
+const bold = 700;
+const classAttribute = 'class';
+const isSelected = 'is-selected';
+
+export {text, bold, backGroundColor, color, errorColor, neutralColor, productTitle, successColor, textLocked,
+    warningColor, fontWeight, classAttribute, isSelected}
